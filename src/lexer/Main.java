583796d2bd1cca78ac64bb30eb9aaa3d6cfaf169
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-12 00:29:33 +0400

    Expand tag list

--- src/lexer/Main.java
+++ src/lexer/Main.java
@@ -17,6 +17,24 @@ public class Main {
                 case Tag.FOR:
                     System.out.println("FOR");
                     break;
+                case Tag.LESS:
+                    System.out.println("OP: <");
+                    break;
+                case Tag.LESS_EQUALS:
+                    System.out.println("OP: <=");
+                    break;
+                case Tag.EQUALS:
+                    System.out.println("OP: ==");
+                    break;
+                case Tag.NOT_EQUALS:
+                    System.out.println("OP: !=");
+                    break;
+                case Tag.BIGGER_EQUALS:
+                    System.out.println("OP: >=");
+                    break;
+                case Tag.BIGGER:
+                    System.out.println("OP: >");
+                    break;
                 default:
                     System.out.println((char)t.tag);
                     break;
--- src/lexer/Tag.java
+++ src/lexer/Tag.java
@@ -4,4 +4,12 @@ public class Tag {
     public static final int NUM = 256;
     public static final int ID = 257;
     public static final int FOR = 258;
+    public static final int TRUE = 259;
+    public static final int FALSE = 260;
+    public static final int LESS = 261;
+    public static final int LESS_EQUALS = 262;
+    public static final int EQUALS = 263;
+    public static final int NOT_EQUALS = 264;
+    public static final int BIGGER_EQUALS = 265;
+    public static final int BIGGER = 266;
 }
