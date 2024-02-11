9518e4f2b17de1b38819901c8aa706f7dd0a76b6
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-12 00:20:28 +0400

    Format code

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -4,15 +4,25 @@ import java.io.IOException;
 import java.util.Hashtable;
 
 public class Lexer {
+
     private char prev = ' ';
     private char peek = ' ';
+
     private Hashtable words = new Hashtable();
-    void reserve(Word t) {
+
+    private void reserve(Word t) {
         words.put(t.lexeme, t);
     }
+
+    private void read() throws IOException {
+        prev = peek;
+        peek = (char) System.in.read();
+    }
+
     public Lexer() {
         reserve(new Word(Tag.FOR, "for"));
     }
+
     public Token scan() throws IOException {
 
         while (true) {
@@ -30,35 +40,44 @@ public class Lexer {
                 } else if (peek == '*') {
                     read();
                     read();
+
                     while (!(prev == '*' && peek == '/')) {
                         read();
                     }
+
                     read();
                 } else {
                     return new Token('/');
                 }
-            } else break;
+            } else {
+                break;
+            }
         }
 
         if (Character.isDigit(peek)) {
             int v = 0;
+
             do {
                 v = 10 * v + Character.digit(peek, 10);
                 read();
             } while (Character.isDigit(peek));
+
             return new Num(v);
         }
 
         if (Character.isLetter(peek)) {
             StringBuffer b = new StringBuffer();
+
             do {
                 b.append(peek);
                 read();
             } while (Character.isLetterOrDigit(peek));
+
             String s = b.toString();
             Word w = (Word)words.get(s);
             if (w != null)
                 return w;
+
             w = new Word(Tag.ID, s);
             words.put(s, w);
             return w;
@@ -67,10 +86,5 @@ public class Lexer {
         Token t = new Token(peek);
         peek = ' ';
         return t;
-    }
-
-    private void read() throws IOException {
-        prev = peek;
-        peek = (char) System.in.read();
     }
 }
