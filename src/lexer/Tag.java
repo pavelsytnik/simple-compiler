b0f0dda33e50c8e4f58405df6bd3b7bc896a0fa8
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-14 22:03:02 +0400

    Make tag order more logical; Add if tag

--- src/lexer/Tag.java
+++ src/lexer/Tag.java
@@ -2,15 +2,16 @@ package lexer;
 
 public class Tag {
     public static final int NUM = 256;
-    public static final int ID = 257;
-    public static final int FOR = 258;
-    public static final int TRUE = 259;
-    public static final int FALSE = 260;
-    public static final int LESS = 261;
-    public static final int LESS_EQUALS = 262;
-    public static final int EQUALS = 263;
-    public static final int NOT_EQUALS = 264;
-    public static final int BIGGER_EQUALS = 265;
-    public static final int BIGGER = 266;
-    public static final int REAL = 267;
+    public static final int REAL = 257;
+    public static final int ID = 258;
+    public static final int IF = 259;
+    public static final int FOR = 260;
+    public static final int TRUE = 261;
+    public static final int FALSE = 262;
+    public static final int LESS = 263;
+    public static final int LESS_EQUALS = 264;
+    public static final int EQUALS = 265;
+    public static final int NOT_EQUALS = 266;
+    public static final int BIGGER = 267;
+    public static final int BIGGER_EQUALS = 268;
 }
