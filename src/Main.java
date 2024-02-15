cfc8ef6d71f04edffde40c64439a8b4e33de0f92
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-15 19:41:59 +0400

    Add prompt for input

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.print("infix2postfix: ");
         InfixToPostfixParser parser = new InfixToPostfixParser();
         parser.expr(); System.out.write('\n');
 
+        System.out.print("task_a: ");
         TaskA taskA = new TaskA();
         taskA.s();
         System.out.write('\n');
 
+        System.out.print("task_b: ");
         TaskB taskB = new TaskB();
         taskB.s();
         System.out.write('\n');
